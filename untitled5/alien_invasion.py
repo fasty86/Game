6b714d5d5575684e78724c0e787ae46996c8f0ca
fasty86 <oleg.tolm1986@gmail.com>
2020-01-20 17:30:16 +0300

    test

--- untitled5/alien_invasion.py
+++ untitled5/alien_invasion.py
@@ -1,6 +1,9 @@
 import sys
 
 import pygame
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """" Overall class to manage game assets and behavior """
@@ -8,24 +11,35 @@ class AlienInvasion:
     def __init__(self):
         """" Initialize the game, and create game resources """
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        settings = Settings()
+        self.screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
         pygame.display.set_caption("Igor Invasion")
         # Set the background color.
-        self.bg_color = (230, 230, 230)
+        self.bg_color = settings.bg_color
+        # Initialize our ship
+        self.ship = Ship(self)
 
     def run_game(self):
         """" Start the main loop for the game. """
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
+            self._check_events()
             # Redrow the screen during each pass through the loop.
             self.screen.fill(self.bg_color)
+            # show ship
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        # Respond to keypresses and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+
+
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
---
+++ untitled5/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """ A class to storer settings """
+
+    def __init__(self):
+        """Initialize the game setttings"""
+        # Screeen setttings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
